7d164d46c6fcaafe94e05ee9bd3259765888c8e4
Raj Shekhar Yadav <147932780+Rajshekharyadav@users.noreply.github.com>
2025-10-30 15:39:49 +0530

    Implement LoginServlet for user authentication

---
+++ LoginServlet.java
@@ -0,0 +1,17 @@
+// LoginServlet.java
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if ("admin".equals(username) && "admin123".equals(password)) {
+            response.sendRedirect("welcome.html");
+        } else {
+            response.getWriter().println("Invalid credentials");
+        }
+    }
+}
